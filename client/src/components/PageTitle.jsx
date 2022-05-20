f1170a0325cc011bc1d5eae8ff263bcd2d5ec2f7
Marko <marko.nikolas@marbleit.rs>
2022-05-20 09:49:42 +0200

    Update page title & subtitle size

--- client/src/components/PageTitle.jsx
+++ client/src/components/PageTitle.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 function PageTitle({ title, desc }) {
 	return (
-		<div className='text-center'>
-			<h1 className='text-3xl text-gray-800 font-semibold'>{title}</h1>
-			<p className='mt-3 text-gray-500'>{desc}</p>
+		<div className='text-left'>
+			<h1 className='text-7xl text-gray-800 font-semibold'>{title}</h1>
+			<p className='mt-3 text-xl text-gray-500'>{desc}</p>
 		</div>
 	);
 }
