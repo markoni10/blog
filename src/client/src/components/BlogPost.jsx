178c927f97a5bc65af76c341a6227f9d7589f67a
Marko Nikolaš <n1-dev+github@simplelogin.co>
2022-07-08 14:25:02 +0200

    Render posts from the database

--- src/client/src/components/BlogPost.jsx
+++ src/client/src/components/BlogPost.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function BlogPost({ items }) {
 	return (
 		<article className='max-w-md mx-auto mt-4 shadow-md rounded-md duration-300 hover:shadow-xl cursor-pointer'>
-			<a href={items.href}>
+			<a href='/'>
 				<img
 					src={items.img}
 					loading='lazy'
@@ -12,20 +12,20 @@ function BlogPost({ items }) {
 				/>
 				<div className='flex items-center mt-2 pt-3 ml-4 mr-2'>
 					<div className='flex-none w-10 h-10 rounded-full'>
-						<img
+						{/* <img
 							src={items.authorLogo}
 							className='flex items-center justify-center overflow-hidden bg-gray-300 before:text-gray-400 before:text-xs before:text-center before:opacity-0 w-full h-full rounded-full'
 							alt={items.authorName}
-						/>
+						/> */}
 					</div>
 					<div className='ml-3'>
-						<span className='block text-gray-900'>{items.authorName}</span>
-						<span className='block text-gray-400 text-sm'>{items.date}</span>
+						{/* <span className='block text-gray-900'>{items.authorName}</span> */}
+						<span className='block text-gray-400 text-sm'>{items.date_created}</span>
 					</div>
 				</div>
 				<div className='pt-3 ml-4 mr-2 mb-3'>
 					<h3 className='text-xl text-gray-900'>{items.title}</h3>
-					<p className='text-gray-400 text-sm mt-1'>{items.desc}</p>
+					<p className='text-gray-400 text-sm mt-1'>{items.excrept}</p>
 				</div>
 			</a>
 		</article>
