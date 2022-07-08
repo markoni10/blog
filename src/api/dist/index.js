1326108e4061169d48dae1c7bbf652e576e311bc
Marko Nikolaš <n1-dev+github@simplelogin.co>
2022-07-09 00:29:21 +0200

    Add typescript

---
+++ src/api/dist/index.js
@@ -0,0 +1,20 @@
+import express, { json } from 'express';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import cors from 'cors';
+const app = express();
+app.use(cors());
+app.use(express.json());
+// routers
+import indexRouter from './routes/index.js';
+import postsRouter from './routes/post/index.js';
+import usersRouter from './routes/user/index.js';
+app.use('/', indexRouter);
+app.use('/posts', postsRouter);
+app.use('/users', usersRouter);
+app.use(logger('dev'));
+app.use(cookieParser());
+app.use(json());
+const port = process.env.PORT || 5000;
+app.listen(port);
+console.log('App is listening on port ' + port);
