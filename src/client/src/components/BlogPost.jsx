dc12e5c33c319af2e53566291dd31baf78c2ed68
Marko Nikolaš <n1-dev+github@simplelogin.co>
2022-07-08 14:29:30 +0200

    Update single blog post render

--- src/client/src/components/BlogPost.jsx
+++ src/client/src/components/BlogPost.jsx
@@ -8,24 +8,24 @@ function BlogPost({ items }) {
 					src={items.img}
 					loading='lazy'
 					alt={items.title}
-					className='flex items-center justify-center w-full h-48 rounded-t-md bg-gray-200 before:text-gray-300 before:font-bold before:text-2xl before:tracking-wide'
+					className='flex items-center justify-center w-full h-48 rounded-t-md bg-gray-200 before:text-gray-300 before:font-bold before:text-2xl before:tracking-wide object-cover'
 				/>
 				<div className='flex items-center mt-2 pt-3 ml-4 mr-2'>
 					<div className='flex-none w-10 h-10 rounded-full'>
 						<img
-							src={items.authorLogo}
+							src=''
 							className='flex items-center justify-center overflow-hidden bg-gray-300 before:text-gray-400 before:text-xs before:text-center before:opacity-0 w-full h-full rounded-full'
-							alt={items.authorName}
+							alt=''
 						/>
 					</div>
 					<div className='ml-3'>
-						<span className='block text-gray-900'>{items.authorName}</span>
-						<span className='block text-gray-400 text-sm'>{items.date}</span>
+						<span className='block text-gray-900'>Author</span>
+						<span className='block text-gray-400 text-sm'>{items.date_created}</span>
 					</div>
 				</div>
 				<div className='pt-3 ml-4 mr-2 mb-3'>
 					<h3 className='text-xl text-gray-900'>{items.title}</h3>
-					<p className='text-gray-400 text-sm mt-1'>{items.desc}</p>
+					<p className='text-gray-400 text-sm mt-1'>{items.excerpt}</p>
 				</div>
 			</a>
 		</article>
