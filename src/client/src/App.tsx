b1f7180406a3e7ff7adf2273b60210ea5e44394f
Marko Nikolaš <n1-dev+github@simplelogin.co>
2022-07-26 12:09:09 +0200

    Add typescript support for client

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,25 +1,19 @@
-import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import Header from './components/Header';
 import Blog from './components/Blog';
-import Preferences from './components/Preferences';
-import Dashboard from './components/Dashboard';
 import Login from './components/Login';
 import Signup from './components/Signup';
 
 function App() {
-	const [token, setToken] = useState();
 
 	return (
 		<>
 			<Header logo={'Awesome Blog'} />
 			<Routes>
-				<Route index path='/' element={<Blog />} />
+				<Route path='/' element={<Blog />} />
 				<Route path='/login' element={<Login />} />
 				<Route path='/signup' element={<Signup />} />
-				<Route path='/dashboard' element={<Dashboard token={token} handleLogin={<Login setToken={setToken} />} />} />
-				<Route path='/preferences' element={<Preferences token={token} />} />
 			</Routes>
 		</>
 	);
--- src/client/src/index.tsx
+++ src/client/src/index.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './assets/stylesheets/tailwind.css';
-import './assets/stylesheets/global.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom'
+import './assets/stylesheets/global.css';
+import './assets/stylesheets/tailwind.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
-	<React.StrictMode>
-		<Router>
-			<App />
-		</Router>
-	</React.StrictMode>,
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
